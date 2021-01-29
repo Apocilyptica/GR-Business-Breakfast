b8f2e611712396a531db283f3bf0cb6a6825adc2
James Jager <jviper882000@gmail.com>
2021-01-28 22:32:01 -0500

    first commit

---
+++ src/redux/rootReducer.js
@@ -0,0 +1,15 @@
+import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+// Reducers
+
+export const rootReducer = combineReducers({});
+
+const configStorage = {
+  key: "root",
+  storage,
+  whitelist: [],
+};
+
+export default persistReducer(configStorage, rootReducer);
